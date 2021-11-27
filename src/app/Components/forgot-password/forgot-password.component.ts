448912ae4957d4af23d0efaa645fcea88ed2e055
Sandeepshinde630 <sandeepshinde630@gmail.com>
2021-11-27 08:50:18 +0530

    [Sandeep]Add: Completed Forgot Password UI

--- src/app/Components/forgot-password/forgot-password.component.ts
+++ src/app/Components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
-
+  ForgotPasswordForm!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.ForgotPasswordForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    })
   }
 
 }
